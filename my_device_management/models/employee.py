704e9ffc76b28570532a3cd5ce2f10866c56c04d
Aastha Joshi <aastha.joshi@ksolves.com>
2024-01-23 17:24:15 +0530

    database files changed

--- my_device_management/models/employee.py
+++ my_device_management/models/employee.py
@@ -8,9 +8,8 @@ class Device_Management(models.Model):
     device_ids = fields.One2many('device.assignment', 'employee_id', string='Devices')
 
     def action_done(self):
-        # res = super().action_confirm()
         connection = sql_db.db_connect('odoo17_school_mgm')
         with contextlib.closing(connection.cursor()) as cr:
-            cr.autocommit(True)
+            cr._cnx.autocommit = True
             env = api.Environment(cr, SUPERUSER_ID, {})
-            env['hr.employee'].create({'name': self.name})
+            env['hr.employee'].create({'name': self.name ,'mobile_phone':self.mobile_phone, 'work_phone':self.work_phone})
